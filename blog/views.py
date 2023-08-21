e23843059ab8a59272e218d5cca778fef55a58f2
hesan76 <hesan.adeli@gmail.com>
2023-08-21 13:18:57 +0330

    transfer data to web pages
    transferred data to web pages with dynamic tools in render and contextx

--- blog/views.py
+++ blog/views.py
@@ -5,4 +5,7 @@ def blog_view(request):
     return render(request, 'blog/blog-home.html')
 
 def blog_single(request):
-    return render(request, 'blog/blog-single.html')
+    context = {'title':'bitcoin crashed again!', 
+                'content':'bitcoin was flying but now grounded as allways', 
+                'author': 'Hesan Adeli'}
+    return render(request, 'blog/blog-single.html', context)
