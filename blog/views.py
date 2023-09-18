c7e60f443bc4d3558b1256e534957bbfab49e034
hesan76 <hesan.adeli@gmail.com>
2023-09-18 11:36:53 +0330

    Add TemplateTag
    Template tags include: inclusion, simple, search, filtering, ...

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render, get_object_or_404
 from blog.models import Post
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
 # Create your views here.
-def blog_view(request):
+def blog_view(request, **kwargs):
     posts = Post.objects.filter(status=1)
+
+    if kwargs.get('cat_name') != None:
+        posts = posts.filter(category__name=kwargs['cat_name'])
+    if kwargs.get('author_username') != None:
+        posts = posts.filter(author__username=kwargs['author_username'])
+
+    posts = Paginator(posts, 3)
+    try:
+        page_number = request.GET.get('page')
+        posts = posts.get_page(page_number)
+    except PageNotAnInteger:
+        posts = posts.get_page(1)
+    except EmptyPage:
+        posts = posts.get_page(1) 
+
     context = {'posts': posts}
     return render(request, 'blog/blog-home.html', context)
 
@@ -18,10 +34,25 @@ def blog_single(request, pid):
     context = {'post':post}
     return render(request, 'blog/blog-single.html', context)
 
-def test(request, pid):
+def blog_category(request, cat_name):
+    posts = Post.objects.filter(status=1)
+    posts = posts.filter(category__name=cat_name)
+    context = {'posts': posts}
+    return render(request, 'blog/blog-home.html', context)
+
+def blog_search(request):
+    # print(request.__dict__)
+    posts = Post.objects.filter(status=1)
+    if request.method == 'GET':
+        if s := request.GET.get('s'):
+            posts = posts.filter(content__contains=s)
+    context = {'posts': posts}
+    return render(request, 'blog/blog-home.html', context)
+
+def test(request):
     # posts = Post.objects.filter(status=0)
     # posts = Post.objects.all()
     # posts = Post.objects.get(id=pid)
-    posts = get_object_or_404(Post, pk=pid)
-    context = {'post':posts}
-    return render(request, 'test.html', context) 
+    # posts = get_object_or_404(Post, pk=pid)
+    # context = {'post':posts}
+    return render(request, 'test.html') 
