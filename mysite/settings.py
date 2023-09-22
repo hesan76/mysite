f8c5a8aba45e3b406b86d0fd073c7ba6119f1356
hesan76 <hesan.adeli@gmail.com>
2023-09-22 22:55:25 +0330

    Add django modules
    Modules: humanize, extension, sites, sitemap, Robots, debugger toolbar, taggit, captcha

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,15 +31,40 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'multi_captcha_admin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
+    'django.contrib.sites',
+    'django.contrib.sitemaps',
+    'captcha',
+    'django_summernote',
+    'robots', 
+    'debug_toolbar',
+    'taggit',
+
     'website.apps.WebsiteConfig',
     'blog'
 ]
+
+# sites frameworks
+SITE_ID = 2
+
+# robots
+ROBOTS_USE_HOST = True  
+ROBOTS_USE_SITEMAP = True
+
+# summernote configs
+SUMMERNOTE_THEME = 'bs4'
+
+# captcha admin setting
+MULTI_CAPTCHA_ADMIN = {
+        'engine': 'simple-captcha',
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -49,6 +74,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'mysite.urls'
@@ -133,3 +159,9 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+INTERNAL_IPS = [
+    '127.0.0.1',
+]
+
+X_FRAME_OPTIONS = 'SAMEORIGIN'
